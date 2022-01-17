32068de72d17307b62a350f2f8c13df58a203292
Dilaa <66761532+amdessential@users.noreply.github.com>
2022-01-18 03:45:28 +0700

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const primaryColor = Color(0XFFC5E1A5);
+  static const primaryDarkColor = Color(0XFF558B2F);
+  static const underlinedTextField = Color(0XFFF1E8E9);
+  static const hintColor = Color(0XFF8BC34A);
+}
---
+++ login_view.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/screens/register_view.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorPalette.primaryColor,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _iconLogin(),
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconLogin() {
+    return Image.asset(
+      "assets/images/agreener.png",
+      width: 150.0,
+      height: 150.0,
+    );
+  }
+
+  Widget _titleDescription() {
+    return Column(
+      children: <Widget>[
+        Padding(padding: EdgeInsets.only(top: 16.0)),
+        Text(
+          "Login into app",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlinedTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlinedTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Password",
+            hintStyle: TextStyle(color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          obscureText: true,
+          autofocus: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        RaisedButton(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: Text(
+            'Login',
+            style: TextStyle(color: ColorPalette.primaryColor),
+            textAlign: TextAlign.center,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, "/");
+          },
+        ),
+        /*InkWell(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            width: double.infinity,
+            child: Text(
+              'Login',
+              style: TextStyle(color: ColorPalette.primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ),*/
+        Padding(
+          padding: EdgeInsets.only(top: 16.0),
+        ),
+        Text(
+          'or',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Register',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, RegisterPage.routeName);
+          },
+        ),
+      ],
+    );
+  }
+}
